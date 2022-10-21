df856ebe11fce2fcc27460623d8dce07b0e9ffa1
Reza Sarijlou <u0154105@utah.edu>
2022-10-21 04:41:07 -0600

    pong who

--- controllers/ping/ping_controller.go
+++ controllers/ping/ping_controller.go
@@ -1,23 +1,13 @@
 package ping
 
-
-
 import (
+	"net/http"
 
-    "net/http"
-
-
-
-    "github.com/gin-gonic/gin"
-
+	"github.com/gin-gonic/gin"
 )
-
-
 
 func Ping(c *gin.Context) { //Need to send the pointer for the library to know what to do
 
-    c.String(http.StatusOK, "pong")
-
-
+	c.String(http.StatusOK, "pong who")
 
 }
